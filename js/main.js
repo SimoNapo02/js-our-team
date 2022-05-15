e9d056c65149e0019502316a8d8aa4178e4b7b3c
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-15 13:51:17 +0200

    aggiunto namecard team

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// aggiungo le nemacard dei dipendenti
+
+const teamCards = [
+    {
+        nome: "Wayne Barnett",
+        img: "img/wayne-barnett-founder-ceo.jpg",
+        posizione: "Founder & CEO",
+    },
+    {
+        nome: "Angela Caroll",
+        img: "img/angela-caroll-chief-editor.jpg",
+        posizione: "Chief Editor",
+    },
+    {
+        nome: "Walter Gordon",
+        img: "img/walter-gordon-office-manager.jpg",
+        posizione: "Office Manager",
+    },
+    {
+        nome: "Angela Lopez",
+        img: "img/angela-lopez-social-media-manager.jpg",
+        posizione: "Social Media Manager",
+    },
+    {
+        nome: "Scott Estrada",
+        img: "img/scott-estrada-developer.jpg",
+        posizione: "Developer",
+    },
+    {
+        nome: "Barbara Ramos",
+        img: "img/barbara-ramos-graphic-designer.jpg",
+        posizione: "Graphic Designer",
+    },
+];
